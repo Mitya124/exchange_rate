d4969240bf40ef63b77b0b36057c70a4ae6e3030
dm88i <dm88ik@gmail.com>
2024-10-21 16:04:01 +0700

    Доработан интерфейс программы

--- Ex_Rate.py
+++ Ex_Rate.py
@@ -5,19 +5,20 @@ from tkinter import ttk
 
 
 def generation_list_rates():
-    answer = requests.get('https://v6.exchangerate-api.com/v6/daa09e68c25ef3ff1812b017/latest/USD')
+    answer = requests.get(f'https://v6.exchangerate-api.com/v6/daa09e68c25ef3ff1812b017/latest/RUB')
     json_info = answer.json()
     list_currency = list(json_info['conversion_rates'].keys())
     return list_currency
 
 def func_exchange():
-    code = combo.get()
-    if code:
-        answer = requests.get('https://v6.exchangerate-api.com/v6/daa09e68c25ef3ff1812b017/latest/USD')
+    code_target = combo_to.get()
+    code_base = combo_from.get()
+    if code_target and code_base:
+        answer = requests.get(f'https://v6.exchangerate-api.com/v6/daa09e68c25ef3ff1812b017/latest/{code_base}')
         json_info = answer.json()
-        if code in json_info['conversion_rates']:
-            rez = json_info['conversion_rates'][code]
-            content_l.config(text=f'1 RUB - {rez} {code}')
+        if code_target in json_info['conversion_rates']:
+            rez = json_info['conversion_rates'][code_target]
+            content_l.config(text=f'1 {code_base} - {rez} {code_target}')
             content_l.config(bg='green')
         else:
             content_l.config(text='Такого кода валюты не существует')
@@ -32,15 +33,19 @@ window = Tk()
 window.title('Курс валют')
 window.geometry('400x400')
 
+combo_from = ttk.Combobox(window, values=generation_list_rates())
+combo_from.pack(pady=[10,10])
 
-
-combo = ttk.Combobox(window, values=generation_list_rates())
-combo.pack(pady=[10,10])
-
-content_l = Label(window, text='Выберите код валюты')
+content_l = Label(window, text='Выберите код базовой валюты')
 content_l.pack(pady=[10,10])
 
-btn = Button(text='Получить курс рубля', command=func_exchange)
+combo_to = ttk.Combobox(window, values=generation_list_rates())
+combo_to.pack(pady=[10,10])
+
+content_l_to = Label(window, text='Выберите код конечной валюты')
+content_l_to.pack(pady=[10,10])
+
+btn = Button(text='Конвертировать', command=func_exchange)
 btn.pack(pady=[10,10])
 
 
